c74337fc0b14908150369a25a29820224de4c0ac
Zhijiang Li <zl256@cornell.edu>
2022-04-04 23:59:12 -0700

    Add basic exception handling

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,15 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    rust_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("no crash!");
     loop {}
 }
 
