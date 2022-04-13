6b31f874d9ce3d40ff33d610d886f9d02694f438
Zhijiang Li <zl256@cornell.edu>
2022-04-13 00:20:38 -0700

    Page table support with frame allocators

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,32 @@
 
 use rust_os::println;
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_os::memory::BootInfoFrameAllocator;
+    use rust_os::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     println!("Hello World{}", "!");
 
     rust_os::init();
 
-    fn stack_overflow() {
-        stack_overflow(); // for each recursion, the return address is pushed
-    }
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
-    // stack_overflow();
-    use x86_64::registers::control::Cr3;
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)}
 
     #[cfg(test)]
     test_main();
